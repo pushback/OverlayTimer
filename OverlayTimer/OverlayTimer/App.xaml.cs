5a9fcfb5afadd8b204488d5dfecf989004f05d96
Jhon Smith(push_back) <jhonsmithworks@gmail.com>
2018-06-03 13:14:27 +0900

    起動オプションに対応。#RRGGBB 不透明度(0.0～1.0)を指定可能にする

--- OverlayTimer/OverlayTimer/App.xaml.cs
+++ OverlayTimer/OverlayTimer/App.xaml.cs
@@ -13,5 +13,21 @@ namespace OverlayTimer
     /// </summary>
     public partial class App : Application
     {
+        public static string[] Args { get; private set; }
+        public static string TextColor { get; private set; } = "Black";
+        public static double Opacity{ get; private set; } = 0.2;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // オプション数をチェック
+            if (e.Args.Length != 2) return;
+
+            // テキストの色
+            TextColor = e.Args[0];
+
+            // テキストの透明度
+            double ope;
+            if(Double.TryParse(e.Args[1], out ope)) Opacity = ope;
+        }
     }
 }
