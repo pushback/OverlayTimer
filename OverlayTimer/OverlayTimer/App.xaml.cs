b91111febc27085172a2211978a97f7c317c3c67
Jhon Smith(push_back) <jhonsmithworks@gmail.com>
2018-06-03 13:33:05 +0900

    起動オプションを#AARRGGBB形式に変更

--- OverlayTimer/OverlayTimer/App.xaml.cs
+++ OverlayTimer/OverlayTimer/App.xaml.cs
@@ -14,20 +14,15 @@ namespace OverlayTimer
     public partial class App : Application
     {
         public static string[] Args { get; private set; }
-        public static string TextColor { get; private set; } = "Black";
-        public static double Opacity{ get; private set; } = 0.2;
+        public static string TextColor { get; private set; } = "#33000000";
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             // オプション数をチェック
-            if (e.Args.Length != 2) return;
+            if (e.Args.Length != 1) return;
 
-            // テキストの色
+            // テキストの色と不透明度(#AARRGGBB形式)
             TextColor = e.Args[0];
-
-            // テキストの透明度
-            double ope;
-            if(Double.TryParse(e.Args[1], out ope)) Opacity = ope;
         }
     }
 }
